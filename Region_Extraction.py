1061ec34166a075cae6334c4bd9533ae54be090c
Javier <javier.ramirez1@upr.edu>
2019-11-03 12:24:18 -0400

    Fixed Bugs

--- Region_Extraction.py
+++ Region_Extraction.py
@@ -3,11 +3,8 @@ import numpy as np
 import math
 import os
 
-n_regions = 0 # for test purposes
-
-# Added This
-regions_list = dict()
-
+# for test purposes
+n_regions = 0
 
 def resize_image(img, scale):
     """
@@ -17,21 +14,18 @@ def resize_image(img, scale):
     :return:
     """
 
-    # # Load image
-    # img = cv2.imread(img_path)
-
     # Obtain parameters for scaling
-    height, width, depth = img.shape
-    imgScale = scale / width
-    newX, newY = img.shape[1] * imgScale, img.shape[0] * imgScale
+    height, width = img.shape[0], img.shape[1]
+    img_scale = scale / width
+    new_x, new_y = img.shape[1] * img_scale, img.shape[0] * img_scale
 
     # Rescale the image
-    return cv2.resize(img, (int(newX), int(newY))), imgScale
+    return cv2.resize(img, (int(new_x), int(new_y))), img_scale
 
 
 def rescale_coordinates(coordinates, scale):
     """
-    Resize the coodinates with the given scale
+    Resize the coordinates with the given scale
     :param coordinates: List of coordinates [x, y, w, h]
     :param scale: Number that resizes the coordinates
     :return: List containing the resized coodinates
@@ -62,6 +56,7 @@ def binarize_image(img):
 
     return thresh
 
+
 def get_centroid(img):
     """
     Given an image return the center of the image.
@@ -70,30 +65,30 @@ def get_centroid(img):
     """
 
     # calculate moments of binary image
-    M = cv2.moments(img)
+    m = cv2.moments(img)
 
     # calculate x,y coordinate of center
-    cX = int(M["m10"] / M["m00"])
-    cY = int(M["m01"] / M["m00"])
+    c_x = int(m["m10"] / m["m00"])
+    c_y = int(m["m01"] / m["m00"])
 
-    return cX, cY
+    return c_x, c_y
 
 
-def generate_wedge_mask(img, cX, cY, angle):
+def generate_wedge_mask(img, c_x, c_y, angle):
     """
-    Given an an input image, its center coodinates and an angle, calculate it's correspondent wedge and generate
+    Given an an input image, its center coordinates and an angle, calculate it's correspondent wedge and generate
     an image mask.
     :param img: Input image.
-    :param cX: Center X coordinate of the input image.
-    :param cY: Center Y coordinate of the input image.
+    :param c_x: Center X coordinate of the input image.
+    :param c_y: Center Y coordinate of the input image.
     :param angle: Angle of the desired wedge.
     :return: Image containing the generated mask of the wedge.
     """
 
     # Creating a right triangle with the given angle and coordinates
-    op_side = math.tan(math.radians(angle)) * cX
-    c = [cX, cX * 2, cX * 2]
-    r = [cY, cY, int(cY - op_side)]
+    op_side = math.tan(math.radians(angle)) * c_x
+    c = [c_x, c_x * 2, c_x * 2]
+    r = [c_y, c_y, int(c_y - op_side)]
 
     # Generate map of three points that compose the right triangle
     rc = np.array((c, r)).T
@@ -117,10 +112,10 @@ def extract_wedge(img, bin_img, angle):
     """
 
     # Get the center coordinate (cX, cY) of the image
-    cX, cY = get_centroid(bin_img)
+    cX, cY = get_centroid(img)
 
     # Generate the mask for the wedge
-    wedge_mask = generate_wedge_mask(bin_img, cX, cY, angle)
+    wedge_mask = generate_wedge_mask(img, cX, cY, angle)
 
     # Create a blank canvas and extract Wedge from main image
     wedge = np.zeros_like(img)
@@ -131,8 +126,8 @@ def extract_wedge(img, bin_img, angle):
 
     # Create a mask of the extracted wedge
     img_dilation = cv2.dilate(wedge, None, iterations=14)
-    img_erosion = cv2.erode(img_dilation, None, iterations=14)
-    ret, thresh = cv2.threshold(img_erosion, 125, 255, cv2.THRESH_BINARY)
+    img_erosion = cv2.erode(img_dilation, None, iterations=18)
+    ret, thresh = cv2.threshold(img_erosion, 150, 255, cv2.THRESH_BINARY)
 
     # Find contours for the wedge
     edged = cv2.Canny(thresh, 250, 500)
@@ -146,13 +141,6 @@ def extract_wedge(img, bin_img, angle):
     # Get the coordinates of the bounding rectangle of the wedge.
     x, y, w, h = cv2.boundingRect(cnt)
 
-    # # For testing
-    # cv2.drawContours(wedge,contours,-1,(0,255,0),3)
-    # show_image(resize_image(wedge))
-    # wedge2=wedge.copy()
-    # rect = cv2.rectangle(wedge2, (x, y), (x+w,y+h), (0,255,0), 2)
-    # show_image(resize_image(wedge2))
-
     return wedge[y:y + h, x:x + w], thresh[y:y + h, x:x + w]
 
 
@@ -164,11 +152,11 @@ def find_max_ins_rect(img):
     """
 
     # Resize the image mask(This is to speed up the calculations of largest inscribed rectangle)
-    rmask, rmask_scale = resize_image(img, 175)
+    r_img, r_scale = resize_image(img, 100)
 
     # Extract a channel from the input image(RGB will be the same since image is binarized)
-    data = img[1:, 1:, 1]
-    nrows, ncols = data.shape
+    data = r_img
+    nrows, ncols = data.shape[0], data.shape[1]
     w = np.zeros(dtype=int, shape=data.shape)
     h = np.zeros(dtype=int, shape=data.shape)
     skip = 0
@@ -199,7 +187,7 @@ def find_max_ins_rect(img):
     rect_coor = area_max[1]
 
     # With the calculated scale, rescale the coordinates to obtain coordinates of the original image.
-    return rescale_coordinates(rect_coor, rmask_scale)
+    return rescale_coordinates(rect_coor, r_scale)
 
 
 def rotate_bound(image, angle):
@@ -242,6 +230,11 @@ def extract_rectangle(img, img_mask):
 
     # Get coordinates of the largest inscribed rectangle a given wedge.
     coord = find_max_ins_rect(img_mask)
+    # # Test
+    # wedge = img.copy()
+    # cv2.rectangle(wedge, (coord[0], coord[1]), (coord[2], coord[3]), (0, 255, 0), 2)
+    # sh_img, _ = resize_image(wedge, 500)
+    # show_image(sh_img)
     # With coordinates of rectangle obtain the rectangle from the wedge.
     rect = img[coord[1]:coord[3], coord[0]:coord[2]]
     # Rotate image 90 degrees
@@ -260,7 +253,7 @@ def extract_regions(img, n_rings, wedge_num):
     """
 
     # Extract image shape properties
-    height, width, depth = img.shape
+    height, width = img.shape[0], img.shape[1]
     # Calculate region height based on number rings
     region_height = int(height/n_rings)
     # Define starting points
@@ -282,8 +275,8 @@ def extract_regions(img, n_rings, wedge_num):
         regions_path = store_region(extracted_region, region_name)
         # Increment global n_regions variable
         increment_n_regions()
-        # TODO: Delete this
-        append_regions_dict(region_name, extracted_region)
+
+        # append_regions_dict(region_name, extracted_region)
 
     return regions_path
 
@@ -296,17 +289,18 @@ def store_region(img, img_name):
     :return: Path where the region is stored
     """
 
-    regions_path = '\\regions'
-    full_path = os.path.join(os.getcwd(), regions_path)
+    regions_path = 'regions'
+    cwd = os.getcwd()
+    full_path = os.path.join(cwd, regions_path)
 
     # Check if regions_path exists, if not then create the path
     if not os.path.exists(full_path):
         try:
-            os.mkdir(os.getcwd() + '\\regions')
+            os.mkdir(regions_path)
         except OSError:
             print("Creation of the directory %s failed" % regions_path)
         else:
-            print("Successfully created the directory %s " % regions_path)
+            print("Successfully created the directory: %s " % regions_path)
 
     # Store region image
     try:
@@ -323,15 +317,10 @@ def increment_n_regions():
     n_regions = n_regions+1
 
 
-# ADDED THIS
-def append_regions_dict(regionname, region):
-    global regions_list
-    regions_list.update({regionname: region})
-
-
 def show_image(img):
     """Show on the screen a given image (For debug purposes)"""
-    cv2.imshow('Image', img)
+    resized_img, _ = resize_image(img, 500)
+    cv2.imshow('Image', resized_img)
     cv2.waitKey(0)
 
 
@@ -378,15 +367,15 @@ def region_extraction(bounded_input_image, bounded_binarized_input_image, number
 
 
 if __name__ == "__main__":
-    num_wedges_p_q = 12
+    num_wedges = 12
     num_of_rings = 3
-    dir_path = "C:\\Users\\Caloj\\Desktop\\Sprout_Images"
-    image_type = ".png"
     rgb_image = cv2.imread('control_rgb.jpg')
     bin_image = cv2.imread('control.png')
     bin_img = binarize_image(bin_image)
+    dir_path = "C:/Users/Caloj/Desktop/Sprout_Images"
+    os.chdir(dir_path)
 
-    Regions_Path = region_extraction(rgb_image, bin_image, num_wedges_p_q, num_of_rings)
+    region_extraction(rgb_image, bin_img, num_wedges, num_of_rings)
 
     # Call Fiber Density Module
 
