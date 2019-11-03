e5727b7d2c8602e096e0ab170b56ba113053a721
Abdias <abdias.santiago@upr.edu>
2019-11-02 20:57:45 -0400

    Complete integration connectors (SproutController class)

--- SproutController.py
+++ SproutController.py
@@ -1,115 +1,197 @@
-import sys
+import os
 import time
-import threading
+from PyQt5.QtCore import QThread
 
-# User input data
-in_data = {}
 
 # Output from "Fiber Density and Distribution" Module
-densities = []
-measurement_data = {'avg_outer_diameter': 0,
-                    'avg_inner_diameter': 0,
-                    'area': 0,
-                    'centroid': 0,
-                    'moment_of_inertia': 0}
 
-exitFlag = 0
-test_delay = 2
 
-class SproutController (threading.Thread):
-    def __init__(self, sprout_ui, in_data):
-        threading.Thread.__init__(self)
-        self.sprout_ui = sprout_ui
+# sample for time delay of modules
+test_delay = 1
+
+# module steps return values
+step_enhanced_image = None
+step_bounded_input_image = None
+bounded_binarized_input_image = None
+
+
+# class SproutController (threading.Thread):
+class SproutController (QThread):
+    def __init__(self, ui, in_data):
+        QThread.__init__(self)
+        self.sprout_ui = ui
         self.in_data = in_data
-        self.percent_up = 0
+        self.percent_count = 0
+
+    def __del__(self):
+        self.wait()
 
     def run(self):
-        global densities, measurement_data
+        global step_enhanced_image
+        print("--------------------------------------------")
         print("Sprout Controller: Acquired Input Parameters")
         print("--------------------------------------------")
         # print for testing
-        print("img_path = " + self.in_data['img_path'])
-        print("intermediate_path = " + self.in_data['intermediate_path'])
-        print("units = " + self.in_data['units'])
-        print("num_measurement = " + self.in_data['num_measurement'])
-        print("num_wedges = " + self.in_data['num_wedges'])
-        print("num_rings = " + self.in_data['num_rings'])
-        print("img_dpi = " + self.in_data['img_dpi'])
-        print("enhance = " + str(self.in_data['enhance']))
+        print("User input:")
+        print(" img_path = " + self.in_data['img_path'])
+        print(" intermediate_path = " + self.in_data['intermediate_path'])
+        print(" units = " + self.in_data['units'])
+        print(" num_measurement = " + str(self.in_data['num_measurement']))
+        print(" num_wedges = " + str(self.in_data['num_wedges']))
+        print(" num_rings = " + str(self.in_data['num_rings']))
+        print(" img_dpi = " + str(self.in_data['img_dpi']))
+        print(" enhance = " + str(self.in_data['enhance']))
+
+        print("Current Working Directory:")
+        print(" " + os.getcwd())
 
         if self.in_data['enhance']:
-            self.image_enhancement_module()
-            self.sprout_ui.update_progress_bar()
+            step_enhanced_image = image_enhancement(self.in_data['img_path'])
+            self.update_progress_bar()
 
-        self.image_pre_processing_module()
-        self.sprout_ui.update_progress_bar()
-        self.region_extraction_module()
-        self.sprout_ui.update_progress_bar()
-        self.fiber_density_distribution_module()
-        self.sprout_ui.update_progress_bar()
+        step_bounded_input_image, bounded_binarized_input_image = pre_process_image(self.in_data['num_measurement'],
+            self.in_data['img_dpi'], self.in_data['units'], self.in_data['img_path'], step_enhanced_image)
+        self.update_progress_bar()
 
+        region_extraction(step_bounded_input_image, bounded_binarized_input_image, self.in_data['num_wedges'],
+                          self.in_data['num_rings'])
+        self.update_progress_bar()
+
+        fiber_density_and_distribution(self.in_data['num_wedges'], self.in_data['num_rings'])
+        self.update_progress_bar()
+
+        print(" * Finished Successfully * ")
         return
 
     def update_progress_bar(self):
-
         if self.in_data['enhance']:
-            self.percent_up += 25
-            self.sprout_ui.update_progress_bar(self.percent_up)
+            self.percent_count += 25
         else:
-            self.percent_up += 33
-            if self.percent_up == 99:
-                self.percent_up += 1
-            self.sprout_ui.update_progress_bar(self.percent_up)
+            self.percent_count += 33
+            if self.percent_count == 99:
+                self.percent_count += 1
 
-        return
+        if self.percent_count >= 100:
+            self.percent_count = 99
 
-    def image_enhancement_module(self):
-        print("\nImage Enhancement Module")
-        print("------------------------")
-        time.sleep(test_delay)
-        return
-
-    def image_pre_processing_module(self):
-        print("\nImage Pre-processing Module")
-        print("---------------------------")
-        time.sleep(test_delay)
+        self.sprout_ui.progressBar.setValue(self.percent_count)
         return
 
 
-    def region_extraction_module(self):
-        print("\nRegion Extraction Module")
-        print("------------------------")
-        time.sleep(test_delay)
-        return
+def image_enhancement(image_path: str):
+    enhanced_image = "\n        _  \n" \
+                       "      /   \\  \n"\
+                       "      \\ _ /    "
 
-    def fiber_density_distribution_module(self):
-        print("\nFiber Density and Distribution")
-        print("------------------------------")
-        time.sleep(test_delay)
-        return
-
-
-def image_enhancement_module():
-    print("\nImage Enhancement Module")
+    print("\n------------------------")
+    print("Image Enhancement Module")
     print("------------------------")
-    time.sleep(test_delay)
-    return
 
-def image_pre_processing_module():
-    print("\nImage Pre-processing Module")
+    print("Input Parameters:")
+    print(" image_path: " + image_path)
+
+    print("Output Data:")
+    print("enhanced_image: " + enhanced_image)
+
+    time.sleep(test_delay)
+    return enhanced_image
+
+
+def pre_process_image(num_of_measurements: int, image_dpi: int, units: str, image_path=None, enhanced_image=None):
+    bounded_input_image = "\n    |   _   |\n" \
+                            "    | /   \\ | \n"\
+                            "    | \\ _ / |    "
+    bounded_binarized_input_image = "black or white"
+
+    print("\n---------------------------")
+    print("Image Pre-processing Module")
     print("---------------------------")
+
+    print("Input Parameters:")
+    print(" num_of_measurements: " + str(num_of_measurements))
+    print(" image_dpi: " + str(image_dpi))
+    print(" units: " + units)
+    print(" image_path: " + str(image_path))
+    print(" enhanced_image: " + str(enhanced_image))
+
+    print("Output Data:")
+    print(" bounded_input_image: " + bounded_input_image)
+    print(" bounded_binarized_input_image: " + bounded_binarized_input_image)
+
     time.sleep(test_delay)
-    return
+    return bounded_input_image, bounded_binarized_input_image
 
 
-def region_extraction_module():
-    print("\nRegion Extraction Module")
+def region_extraction(bounded_input_image: object, bounded_binarized_input_image: object,
+                      number_wedges: int, number_rings: int):
+    print("\n------------------------")
+    print("Region Extraction Module")
     print("------------------------")
+
+    print("Input Parameters:")
+    print(" bounded_input_image: " + bounded_input_image)
+    print(" bounded_binarized_input_image: " + bounded_binarized_input_image)
+    print(" number_wedges: " + str(number_wedges))
+    print(" number_rings: " + str(number_rings))
+
+    print("Output Data:")
+    print(" None ")
+
     time.sleep(test_delay)
     return
 
-def fiber_density_distribution_module():
-    print("\nFiber Density and Distribution")
+
+def fiber_density_and_distribution(number_rings: int, number_wedges: int):
+    print("\n------------------------------")
+    print("Fiber Density and Distribution")
     print("------------------------------")
+
+    print("Input Parameters:")
+    print(" number_rings: " + str(number_rings))
+    print(" number_wedges: " + str(number_wedges))
+
+    print("Output Data:")
+    print(" None ")
+
     time.sleep(test_delay)
     return
+
+
+def get_dimensional_measurements():
+    measurement_data = [22, 8, 6, 3, 3, 188.496, 188.496, -1.3]
+    print("\n-------------------------")
+    print("Get Dimensional Measurement")
+    print("---------------------------")
+
+    print("Input Parameters:")
+    print(" None")
+
+    print("Output Data:")
+    print(" Area: " + str(measurement_data[0]))
+    print(" Avg Outer Diameter: " + str(measurement_data[1]))
+    print(" Avg Inner Diameter: " + str(measurement_data[2]))
+    print(" Centroid x: " + str(measurement_data[3]))
+    print(" Centroid y: " + str(measurement_data[4]))
+    print(" Moment of Inertia x: " + str(measurement_data[5]))
+    print(" Moment of Inertia y: " + str(measurement_data[6]))
+    print(" Product of Inertia: " + str(measurement_data[7]))
+
+    return measurement_data
+
+
+def get_fiber_density():
+    densities = [[0.4500, 0.4400, 0.4000, 0.3800, 0.3500, 0.3900, 0.4500, 0.4200, 0.4100],
+                 [0.5000, 0.5500, 0.4900, 0.4500, 0.5000, 0.5100, 0.4700, 0.5300, 0.5000],
+                 [0.7500, 0.7100, 0.7000, 0.6800, 0.6900, 0.7000, 0.7500, 0.7200, 0.7125],
+                 [0.5667, 0.5667, 0.5300, 0.5033, 0.5133, 0.5333, 0.5567, 0.5567, 0.5408]]
+    print("\n-------")
+    print("Densities")
+    print("---------")
+
+    print("Input Parameters:")
+    print(" None")
+
+    print("Output Data:")
+    print(" densities: " + str(densities))
+    return densities
+
--- SproutUiController.py
+++ SproutUiController.py
@@ -1,8 +1,17 @@
+import ctypes
+import os
+
 from PyQt5 import QtWidgets, uic, QtGui
 import sys
 import _thread
 import threading
+import signal
 import time
+
+# from PyQt5.QtCore import QThread
+# from PyQt5.QtCore import Signal
+from PyQt5.QtCore import pyqtSignal
+
 import SproutController as Sprout
 
 
@@ -15,6 +24,8 @@ from PyQt5.Qt import QGradient, Qt
 # from PySide2.QtCharts import QtCharts
 from PyQt5.QtChart import QChart, QLineSeries, QChartView
 
+window = None
+# thread1 = None
 
 count_pb = 0
 
@@ -26,25 +37,27 @@ rings_messageBox_flag = False
 in_data = {'img_path': "",
            'intermediate_path': "",
            'units': "",
-           'num_measurement': "0",
-           'num_wedges': "0",
-           'num_rings': "0",
-           'img_dpi': "0",
+           'num_measurement': 0,
+           'num_wedges': 0,
+           'num_rings': 0,
+           'img_dpi': 0,
            'enhance': False}
 
 # columns: wedges, rows: rings (r1(w1,w2,...,w7,wAvg),r2(...),r3(...),rAvg(...))
-densities = [[0.4500, 0.4400, 0.4000, 0.3800, 0.3500, 0.3900, 0.4500, 0.4200, 0.4100],
-             [0.5000, 0.5500, 0.4900, 0.4500, 0.5000, 0.5100, 0.4700, 0.5300, 0.5000],
-             [0.7500, 0.7100, 0.7000, 0.6800, 0.6900, 0.7000, 0.7500, 0.7200, 0.7125],
-             [0.5667, 0.5667, 0.5300, 0.5033, 0.5133, 0.5333, 0.5567, 0.5567, 0.5408]]
+densities = []
+# densities = [[0.4500, 0.4400, 0.4000, 0.3800, 0.3500, 0.3900, 0.4500, 0.4200, 0.4100],
+#              [0.5000, 0.5500, 0.4900, 0.4500, 0.5000, 0.5100, 0.4700, 0.5300, 0.5000],
+#              [0.7500, 0.7100, 0.7000, 0.6800, 0.6900, 0.7000, 0.7500, 0.7200, 0.7125],
+#              [0.5667, 0.5667, 0.5300, 0.5033, 0.5133, 0.5333, 0.5567, 0.5567, 0.5408]]
 
-measurement_data = {'avg_outer_diameter': 8,
-                    'avg_inner_diameter': 6,
-                    'area': 22,
-                    'centroid_x': 3,
-                    'centroid_y': 3,
-                    'moment_of_inertia_x': 188.496,
-                    'moment_of_inertia_y': 188.496}
+measurement_data = {} # todo fix wen data is null
+# measurement_data = {'avg_outer_diameter': 8,
+#                     'avg_inner_diameter': 6,
+#                     'area': 22,
+#                     'centroid_x': 3,
+#                     'centroid_y': 3,
+#                     'moment_of_inertia_x': 188.496,
+#                     'moment_of_inertia_y': 188.496}
 
 save_file_file_name = ""
 save_folder_file_path = ""
@@ -64,10 +77,12 @@ class Ui(QtWidgets.QMainWindow):
         self.chartView = None
 
         self.is_running = False
-        self.thread1 = Sprout.SproutController(self, in_data)
 
-        self.ui()
+        self.myThread = None
+        self.in_density = []
+        self.measurement_data = {}
 
+        # self.ui()
 
     def ui(self):
         self.tabWidget_1.setCurrentIndex(0)
@@ -95,7 +110,25 @@ class Ui(QtWidgets.QMainWindow):
         self.comboBox_rings.hide()
         self.comboBox_wedges.hide()
 
+        self.progressBar.valueChanged.connect(self.progress_change)
+
+        self.pushButton.clicked.connect(self.terminate_thread)
+
         self.show()
+
+    def progress_change(self):
+        global densities, measurement_data
+        if self.progressBar.value() == 99:
+            self.myThread.wait()
+            densities = Sprout.get_fiber_density()
+            measurement_data = Sprout.get_dimensional_measurements()
+            self.start_button_func()
+
+    def terminate_thread(self):
+        if self.myThread is not None:
+            print(" - - killing thread")
+            self.myThread.terminate()
+            self.myThread.wait()
 
     def is_int_inbound(self, ui_in: str, lower: int, upper: int, ui_in_name: str):
         if not (str.isdigit(ui_in)) or int(ui_in) > upper or int(ui_in) < lower:
@@ -111,12 +144,12 @@ class Ui(QtWidgets.QMainWindow):
             return
         wedges_messageBox_flag = True
 
-        if self.is_int_inbound(self.lineEdit_numWedges.text(), 4, 100, self.label_numWedges.text()):
+        if self.is_int_inbound(self.lineEdit_numWedges.text(), 3, 100, self.label_numWedges.text()):
             temp = int(self.lineEdit_numWedges.text()) * 4
             self.label_numWedgesFeedback.setText("Num. Wedges: " + str(temp) + " @ {:.1f}º".format(360 / temp))
             self.label_numRegionsFeedback.setText(str(temp * int(in_data['num_rings'])))
             wedges_messageBox_flag = False
-            # in_data['num_wedges'] = str(temp)
+            in_data['num_wedges'] = temp
 
     def update_num_rings(self):
         global in_data, wedges_messageBox_flag, rings_messageBox_flag
@@ -129,7 +162,7 @@ class Ui(QtWidgets.QMainWindow):
             self.label_numRingsFeedback.setText(str(temp))
             self.label_numRegionsFeedback.setText(str(int(temp) * int(in_data['num_wedges'])))
             rings_messageBox_flag = False
-            # in_data['num_rings'] = str(temp)
+            in_data['num_rings'] = temp
 
     def browse_file(self):
         url = QFileDialog.getOpenFileName(self, "Open a file", "", "All Files(*);;*.jpg; *jpeg;; *.png;; *bmp;; *tiff")
@@ -148,17 +181,16 @@ class Ui(QtWidgets.QMainWindow):
 
         # if program is currently in progress
         if self.is_running:
+            # self.thread1.join()
             # if finished successfully
             # count_pb must be set to zero when the fiber density calculation is finished successfully
-            if self.progressBar.value() >= 100:
+            self.terminate_thread()
+            if self.progressBar.value() == 99:
                 self.progressBar.setValue(0)
                 self.dashboard_tab.setEnabled(True)
                 self.tabWidget_2.setEnabled(True)
                 self.graphs_tab.setEnabled(True)
                 self.region_density_tab.setEnabled(True)
-
-                self.comboBox_rings.show()
-                self.comboBox_wedges.show()
 
                 # create graphs
                 self.create_graphs()
@@ -174,44 +206,46 @@ class Ui(QtWidgets.QMainWindow):
 
             self.inputs_set_enabled(True)
 
-            self.start_button.setStyleSheet("background-color: #539844;\nborder: 2px solid #444444;\n"
-                                            "border-radius: 8px;\ncolor:white;\nfont: bold;")
+            self.start_button.setStyleSheet("QPushButton{\nbackground-color: #539844;\nborder: 2px solid #444444;\n"
+                                            "border-radius: 8px;\ncolor:white;\nfont: bold;\n}")
             self.start_button.setText("Start")
             self.progressBar.hide()
             self.label_progressBar.hide()
             self.progressBar.setValue(0)
 
-            count_pb = 0
-
             self.is_running = False
+
         # if program has not started
         else:
+            # todo: uncomment before happy hour
             # if(self.lineEdit_imagePath.text() is "" or self.lineEdit_intermediateStepPath.text() is "" or
             #         self.lineEdit_numWedges.text() is "" or self.lineEdit_numRings.text() is "" or
             #         self.lineEdit_numMeasurements.text() is "" or self.lineEdit_imageDPI.text() is ""):
             #     self.warning_message_box("Make sure all inputs are filled in.")
             #     return
 
-            if not self.is_int_inbound(self.lineEdit_numMeasurements.text(), 1, 25, self.label_numMeasurements.text()):
+            if not self.is_int_inbound(self.lineEdit_numMeasurements.text(), 3, 100, self.label_numMeasurements.text()):
                 return
-            if not self.is_int_inbound(self.lineEdit_numWedges.text(), 4, 100, self.label_numWedges.text()):
+            if not self.is_int_inbound(self.lineEdit_numWedges.text(), 3, 100, self.label_numWedges.text()):
                 return
             if not self.is_int_inbound(self.lineEdit_numRings.text(), 1, 25, self.label_numRings.text()):
                 return
-            if not self.is_int_inbound(self.lineEdit_imageDPI.text(), 25, 2400, self.label_imageDPI.text()):
+            if not self.is_int_inbound(self.lineEdit_imageDPI.text(), 25, 4800, self.label_imageDPI.text()):
                 return
 
+            # todo erase line below before happy hour
+            in_data['intermediate_path'] = "C:/Users/Abdias/Desktop/Sprout"
+            os.chdir(in_data['intermediate_path'])
+
             in_data['units'] = self.comboBox_units.currentText()
-            in_data['num_measurement'] = str(int(self.lineEdit_numMeasurements.text())*4)
-            in_data['num_wedges'] = str(int(self.lineEdit_numWedges.text())*4)
-            in_data['num_rings'] = self.lineEdit_numRings.text()
-            in_data['img_dpi'] = self.lineEdit_imageDPI.text()
+            in_data['num_measurement'] = int(self.lineEdit_numMeasurements.text())*4
+            in_data['img_dpi'] = int(self.lineEdit_imageDPI.text())
             in_data['enhance'] = bool(self.checkBox_imageEnhancement.isChecked())
 
             self.inputs_set_enabled(False)
 
-            self.start_button.setStyleSheet("background-color: #da2a2a;\nborder: 2px solid #444444;\n"
-                                            "border-radius: 8px;\ncolor:white;\nfont: bold;")
+            self.start_button.setStyleSheet("QPushButton{\nbackground-color: #da2a2a;\nborder: 2px solid #444444;\n"
+                                            "border-radius: 8px;\ncolor:white;\nfont: bold;\n}")
             self.start_button.setText("Stop")
             self.progressBar.show()
             self.label_progressBar.show()
@@ -220,8 +254,17 @@ class Ui(QtWidgets.QMainWindow):
             self.is_running = True
 
             # Start Sprout Controller for fiber density calculation
-            self.start_sprout_controller()
-            return
+            try:
+                self.myThread = Sprout.SproutController(self, in_data)
+            except:
+                print("** myThread created failed **")
+            try:
+                self.myThread.start()
+            except :
+                print("** qthread fail **")
+
+    def done(self):
+        print("myTHread is done wiht his f*&# life")
 
     def inputs_set_enabled(self, val):
         self.browse_button_1.setEnabled(val)
@@ -298,6 +341,12 @@ class Ui(QtWidgets.QMainWindow):
         self.chartView = QChartView(self.wedge_chart, self.widget_wedges)
         self.chartView.resize(self.widget_wedges.size())
 
+        self.widget_rings.show()
+        self.widget_wedges.show()
+
+        self.comboBox_rings.show()
+        self.comboBox_wedges.show()
+
     def change_rings_graph(self):
         global default_comboBox_graph_item
 
@@ -371,15 +420,14 @@ class Ui(QtWidgets.QMainWindow):
 
     def set_measurement_data(self):
         global measurement_data
-        self.lineEdit_avgOuterDiameter.setText(str(measurement_data['avg_outer_diameter']) + " " + in_data['units'])
-        self.lineEdit_avgInnerDiameter.setText(str(measurement_data['avg_inner_diameter']) + " " + in_data['units'])
-        self.lineEdit_area.setText(str(measurement_data['area']) + " " + in_data['units'] + "^2")
-        self.lineEdit_centroid_x.setText(str(measurement_data['centroid_x']) + " " + in_data['units'])
-        self.lineEdit_centroid_y.setText(str(measurement_data['centroid_y']) + " " + in_data['units'])
-        self.lineEdit_momentOfInertia_x.setText(str(measurement_data['moment_of_inertia_x'])
-                                                + " " + in_data['units'] + "^4")
-        self.lineEdit_momentOfInertia_y.setText(str(measurement_data['moment_of_inertia_y'])
-                                                + " " + in_data['units'] + "^4")
+        self.lineEdit_area.setText(str(measurement_data[0]) + " " + in_data['units'] + "^2")
+        self.lineEdit_avgOuterDiameter.setText(str(measurement_data[1]) + " " + in_data['units'])
+        self.lineEdit_avgInnerDiameter.setText(str(measurement_data[2]) + " " + in_data['units'])
+        self.lineEdit_centroid_x.setText(str(measurement_data[3]) + " " + in_data['units'])
+        self.lineEdit_centroid_y.setText(str(measurement_data[4]) + " " + in_data['units'])
+        self.lineEdit_momentOfInertia_x.setText(str(measurement_data[5]) + " " + in_data['units'] + "^4")
+        self.lineEdit_momentOfInertia_y.setText(str(measurement_data[6]) + " " + in_data['units'] + "^4")
+        self.lineEdit_productOfInertia.setText(str(measurement_data[7]) + " " + in_data['units'] + "^4")
 
     def warning_message_box(self, message):
         global wedges_messageBox_flag, rings_messageBox_flag
@@ -393,49 +441,6 @@ class Ui(QtWidgets.QMainWindow):
         self.windowModality()
         self.save_window_ui.show()
         self.save_window_ui.raise_()
-
-    def start_sprout_controller(self):
-        global in_data, densities, measurement_data, percent_count
-        print("Sprout Controller:")
-        print("------------------")
-        # print for testing
-        print("img_path = " + in_data['img_path'])
-        print("intermediate_path = " + in_data['intermediate_path'])
-        print("units = " + in_data['units'])
-        print("num_measurement = " + in_data['num_measurement'])
-        print("num_wedges = " + in_data['num_wedges'])
-        print("num_rings = " + in_data['num_rings'])
-        print("img_dpi = " + in_data['img_dpi'])
-        print("enhance = " + str(in_data['enhance']))
-
-        percent_count = 0
-        self.progressBar.setValue(percent_count)
-
-        if in_data['enhance']:
-            Sprout.image_enhancement_module()
-            self.progressBar.setValue(self.update_progress_bar())
-
-        Sprout.image_pre_processing_module()
-        self.progressBar.setValue(self.update_progress_bar())
-        Sprout.region_extraction_module()
-        self.progressBar.setValue(self.update_progress_bar())
-        Sprout.fiber_density_distribution_module()
-        self.progressBar.setValue(self.update_progress_bar())
-
-        self.start_button_func()
-        return
-
-    def update_progress_bar(self):
-        global percent_count
-
-        if in_data['enhance']:
-            percent_count += 25
-        else:
-            percent_count += 33
-            if percent_count == 99:
-                percent_count += 1
-
-        return percent_count
 
 
 class SaveWindow(QtWidgets.QMainWindow):
@@ -451,7 +456,6 @@ class SaveWindow(QtWidgets.QMainWindow):
         self.save_button.clicked.connect(self.save_graph_data)
         self.cancel_button.clicked.connect(self.cancel_save_graph_data)
         self.checkBox_graphs.setChecked(True)
-        # self.show()
 
     def browse_folder(self):
         url = QFileDialog.getExistingDirectory(self, "Open a directory", "", QFileDialog.ShowDirsOnly)
@@ -482,11 +486,13 @@ class SaveWindow(QtWidgets.QMainWindow):
 
 
 def main():
+    global window
     app = QtWidgets.QApplication(sys.argv)
+    # thread1 = Sprout.SproutController(in_data)
     window = Ui()
+    window.ui()
     sys.exit(app.exec_())
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
-
